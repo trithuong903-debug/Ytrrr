c106939e46cf40ef9c3b6ac7ef7a8688e8e5fa7e
trithuong903-debug <trithuong903@gmail.com>
2025-09-14 05:56:18 +0700

    script thuong hub
    vì ro tôi tạo

---
+++ Scriptdemo.lua
@@ -0,0 +1,162 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "TBoy Roblox",
+         Animation = "Youtube: TBoy Roblox"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http//www.roblox.com/asset/?id=72006812029040",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Scripts = {
+	{
+		PlacesIds = {2753915549, 4442272183, 7449423635},
+		UrlPath = "BloxFruits.luau"
+	},
+	{
+		PlacesIds = {10260193230},
+		UrlPath = "MemeSea.luau"
+	},
+}
+
+local fetcher, urls = {}, {}
+
+local _ENV = (getgenv or getrenv or getfenv)()
+
+urls.Owner = "https://raw.githubusercontent.com/tlredz/";
+urls.Repository = urls.Owner .. "Scripts/refs/heads/main/";
+urls.Translator = urls.Repository .. "Translator/";
+urls.Utils = urls.Repository .. "Utils/";
+
+do
+	local last_exec = _ENV.rz_execute_debounce
+	
+	if last_exec and (tick() - last_exec) <= 5 then
+		return nil
+	end
+	
+	_ENV.rz_execute_debounce = tick()
+end
+
+do
+	local executor = syn or fluxus
+	local queueteleport = queue_on_teleport or (executor and executor.queue_on_teleport)
+	
+	if not _ENV.rz_added_teleport_queue and type(queueteleport) == "function" then
+		local ScriptSettings = {...}
+		local SettingsCode = ""
+		
+		_ENV.rz_added_teleport_queue = true
+		
+		local Success, EncodedSettings = pcall(function()
+			return game:GetService("HttpService"):JSONEncode(ScriptSettings)
+		end)
+		
+		if Success and EncodedSettings then
+			SettingsCode = "unpack(game:GetService('HttpService'):JSONDecode('" .. EncodedSettings .. "'))"
+		end
+		
+		pcall(queueteleport, ("loadstring(game:HttpGet('%smain.luau'))(%s)"):format(urls.Repository, SettingsCode))
+	end
+end
+
+do
+	if _ENV.rz_error_message then
+		_ENV.rz_error_message:Destroy()
+	end
+	
+	local identifyexecutor = identifyexecutor or (function() return "Unknown" end)
+	
+	local function CreateMessageError(Text)
+		_ENV.loadedFarm = nil
+		_ENV.OnFarm = false
+		
+		local Message = Instance.new("Message", workspace)
+		Message.Text = string.gsub(Text, urls.Owner, "")
+		_ENV.rz_error_message = Message
+		
+		error(Text, 2)
+	end
+	
+	local function formatUrl(Url)
+		for key, path in urls do
+			if Url:find("{" .. key .. "}") then
+				return select(1, Url:gsub("{" .. key .. "}", path))
+			end
+		end
+		
+		return Url
+	end
+	
+	function fetcher.get(Url)
+		local success, response = pcall(function()
+			return game:HttpGet(formatUrl(Url))
+		end)
+		
+		if success then
+			return response
+		else
+			CreateMessageError(`[1] [{ identifyexecutor() }] failed to get http/url/raw: { Url }\n>>{ response }<<`)
+		end
+	end
+	
+	function fetcher.load(Url: string, concat: string?)
+		local raw = fetcher.get(Url) .. (if concat then concat else "")
+		local runFunction, errorText = loadstring(raw)
+		
+		if type(runFunction) ~= "function" then
+			CreateMessageError(`[2] [{ identifyexecutor() }] sintax error: { Url }\n>>{ errorText }<<`)
+		else
+			return runFunction
+		end
+	end
+end
+
+local function IsPlace(Script)
+	if Script.PlacesIds and table.find(Script.PlacesIds, game.PlaceId) then
+		return true
+	elseif Script.GameId and Script.GameId == game.GameId then
+		return true
+	end
+end
+
+for _, Script in Scripts do
+	if IsPlace(Script) then
+		return fetcher.load("{Repository}Games/" .. Script.UrlPath)(fetcher, ...)
+	end
+end
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name =Thuong hub ",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+  end
+  })
